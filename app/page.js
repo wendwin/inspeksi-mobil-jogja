b87655920edb5655a758c368daa6918343d3c376
Aldo Syahputra <aldosyahputra80@gmail.com>
2025-05-02 23:12:59 +0700

    membuat tampilan footer

--- app/page.js
+++ app/page.js
@@ -7,6 +7,7 @@ import Trusted from "./components/landingpagecomponents/Trusted";
 import Service from "./components/landingpagecomponents/Service";
 import Testimoni from "./components/landingpagecomponents/Testimoni";
 import Mitra from "./components/landingpagecomponents/Mitra";
+import Footer from "./components/landingpagecomponents/Footer";
 
 // jika ada compoenent baru maka lakukan import disini sesuikan nama folder dan filenya
 
@@ -20,6 +21,7 @@ export default function Home() {
       <Order />
       <Testimoni />
       <Mitra />
+      <Footer />
     </main>
   );
 }
