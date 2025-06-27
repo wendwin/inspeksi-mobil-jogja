597cdd2c65f65e2653e77c565011a691bc661578
wendwin <wendwin12@gmail.com>
2025-06-28 00:05:33 +0700

    merubah hero

--- app/components/landingpagecomponents/Promotion.js
+++ app/components/landingpagecomponents/Promotion.js
@@ -4,16 +4,16 @@ export default function Promotion(){
           <div className="relative w-full md:w-1/2 md:h-[280]">
             <img src="/img/inspeksi-1.png" alt="" className="w-full h-full object-cover "/>
             <div className="absolute inset-0 flex flex-col items-center justify-center text-white text-center bg-black/25  hover:bg-black/10">
-              <h3 className="text-sm uppercase tracking-widest">Promotion</h3>
-              <h2 className="text-1xl font-bold md:text-2xl">Know Your Car Better</h2>
+              {/* <h3 className="text-sm uppercase tracking-widest">Promotion</h3> */}
+              <h2 className="text-1xl font-bold md:text-2xl">Kenali Mobil Anda dengan Lebih Baik</h2>
             </div>
           </div>
 
           <div className="relative w-full md:w-1/2 md:h-[280]">
             <img src="/img/inspeksi-2.png" alt="" className="w-full h-full object-cover "/>
             <div className="absolute inset-0 flex flex-col items-center justify-center text-white text-center bg-black/25 hover:bg-black/10">
-              <h3 className="text-sm uppercase tracking-widest">Promotion</h3>
-              <h2 className="text-1xl font-bold md:text-2xl">Check Before You Choose</h2>
+              {/* <h3 className="text-sm uppercase tracking-widest">Promotion</h3> */}
+              <h2 className="text-1xl font-bold md:text-2xl">Periksa Sebelum Anda Memilih</h2>
               <div className="f">
               </div>
             </div>
